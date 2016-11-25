de01b33a7b232173470c66a7f0facf8c98dc6085
FranciscoAntonio <FranciscoAntonio@SURFACE>
2016-11-25 17:28:45 +0100

    Agregar archivos de proyecto.

---
+++ webservice/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\FranciscoAntonio\SkyDrive\universidad\TFG\ser\webservice\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
